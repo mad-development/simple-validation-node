cb00e33384f74a1ce3f429b2749f32e8fec067ec
Madson Junior <madson.junior@maddev.net>
2023-06-27 08:40:49 -0300

    feat: add validation by field

--- validation.js
+++ validation.js
@@ -1,17 +1,30 @@
 export default class Validation {
   constructor(validations) {
     this.validations = validations;
+    this.errors = [];
   }
 
-  validate(input) {
-    let errors = [];
+  /**
+   * Check if there are errors on a field
+   * @param field
+   * @return {boolean}
+   */
+  hasErrorsOnField(field) {
+    return this.errors.some(error => error.field === field);
+  }
 
+  /**
+   * Validate all fields and return an array of errors
+   * @param input
+   * @return {[{field: string, message: string}]}
+   */
+  validate(input) {
     for (let field in this.validations) {
       for (let validation of this.validations[field]) {
         let result = validation(input[field]);
 
         if (result !== true) {
-          errors.push({
+          this.errors.push({
             field: field,
             message: result
           });
@@ -19,6 +32,6 @@ export default class Validation {
       }
     }
 
-    return errors;
+    return this.errors;
   }
 }
--- validation.spec.js
+++ validation.spec.js
@@ -2,38 +2,58 @@ import Validation from './validation';
 import rules from './validation-rules';
 
 describe('Validation', function () {
-    it('should test validation without error', function () {
-        const validation = new Validation({
-            'campo1': [rules.required, rules.max(100), rules.min(10), rules.integer],
-            'campo2': [rules.required, rules.text],
-            'campo3': [rules.required, rules.float]
-        });
-
-        const input = {
-            'campo1': 50,
-            'campo2': 'texto',
-            'campo3': 10.5
-        };
-
-        expect(validation.validate(input)).toEqual([]);
+  it('should test validation without error', function () {
+    const validation = new Validation({
+      'campo1': [rules.required, rules.max(100), rules.min(10), rules.integer],
+      'campo2': [rules.required, rules.text],
+      'campo3': [rules.required, rules.float]
     });
-    it('should test validation with error', function () {
-        const validation = new Validation({
-            'campo1': [rules.required, rules.max(100), rules.min(10), rules.integer],
-            'campo2': [rules.required, rules.text],
-            'campo3': [rules.required, rules.float]
-        });
 
-        const input = {
-            'campo1': 50,
-            'campo2': 10,
-            'campo3': 'texto'
-        };
+    const input = {
+      'campo1': 50,
+      'campo2': 'texto',
+      'campo3': 10.5
+    };
 
-        expect(validation.validate(input)).toEqual([
-            {field: 'campo2', message: 'Este campo deve ser texto.'},
-            {field: 'campo3', message: 'Este campo deve ser um número.'}
-        ]);
+    expect(validation.validate(input)).toEqual([]);
+  });
+  it('should test validation with error', function () {
+    const validation = new Validation({
+      'campo1': [rules.required, rules.max(100), rules.min(10), rules.integer],
+      'campo2': [rules.required, rules.text],
+      'campo3': [rules.required, rules.float]
     });
+
+    const input = {
+      'campo1': 50,
+      'campo2': 10,
+      'campo3': 'texto'
+    };
+
+    expect(validation.validate(input)).toEqual([
+      {field: 'campo2', message: 'Este campo deve ser texto.'},
+      {field: 'campo3', message: 'Este campo deve ser um número.'}
+    ]);
+  });
+
+  it('should verify if a field has error', function () {
+    const validation = new Validation({
+      'campo1': [rules.required, rules.max(100), rules.min(10), rules.integer],
+      'campo2': [rules.required, rules.text],
+      'campo3': [rules.required, rules.float]
+    });
+
+    const input = {
+      'campo1': 50,
+      'campo2': 10,
+      'campo3': 'texto'
+    };
+
+    validation.validate(input);
+
+    expect(validation.hasErrorsOnField('campo2')).toBe(true);
+    expect(validation.hasErrorsOnField('campo3')).toBe(true);
+    expect(validation.hasErrorsOnField('campo1')).toBe(false);
+  });
 });
 
