00fba2801d7b94e1aa916a312169550d9d55be22
Madson Junior <madson-jr@ip-192-168-15-13.us-west-2.compute.internal>
2023-06-26 21:00:25 -0300

    feat: add support for boolean validation

--- validation-rules.js
+++ validation-rules.js
@@ -4,6 +4,7 @@ const min = minVal => input => (input >= minVal ? true : `Valor mínimo é ${min
 const text = input => (typeof input === 'string' ? true : 'Este campo deve ser texto.');
 const integer = input => (Number.isInteger(input) ? true : 'Este campo deve ser um número inteiro.');
 const float = input => (typeof input === 'number' ? true : 'Este campo deve ser um número.');
+const boolean = input => (typeof input === 'boolean' ? true : 'Este campo deve ser um booleano.');
 
 module.exports = {
   required,
@@ -11,5 +12,6 @@ module.exports = {
   min,
   text,
   integer,
-  float
+  float,
+  boolean
 };
